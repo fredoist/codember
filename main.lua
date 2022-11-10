776ca4d2d14cc3f932cef42b78723eaffe26bdee
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-11-10 05:34:08 +0000

    Add dynamic solution printing in console

--- main.lua
+++ main.lua
@@ -1,3 +1,8 @@
 local reto1 = require('reto-1/main')
 
-print('Solución al reto no. 1:\n\n\t✅ '..reto1.solve())
+local solutions = { reto1 }
+
+for i=1, #solutions do
+  print('✅ Solución al reto no. '..i..':')
+  print('\n\t'..solutions[i].solve())
+end
